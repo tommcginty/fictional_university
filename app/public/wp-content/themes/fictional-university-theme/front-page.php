d576ebe44a996e3ad1de2e20769ead8077e05745
Tom McGinty <thmcginty@gmail.com>
2020-07-25 11:59:18 -0400

    Adds custom fields

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -24,18 +24,27 @@
         $homepageEvents->the_post(); ?>
         <div class="event-summary">
         <a class="event-summary__date t-center" href="#">
-          <span class="event-summary__month">Mar</span>
-          <span class="event-summary__day">25</span>
+          <span class="event-summary__month"><?php 
+            $eventDate = new DateTime(get_field('event_date'));
+            echo $eventDate -> format('M');
+          ?></span>
+          <span class="event-summary__day"><?php echo $eventDate -> format('d');
+?></span>
         </a>
         <div class="event-summary__content">
           <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-          <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+          <p><?php  if (has_excerpt()) {
+          echo get_the_excerpt();
+        } else {
+          echo wp_trim_words(get_the_content(), 18);
+        }
+         ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
         </div>
       </div>
       <?php }
     ?>
 
-    <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+    <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event')?>" class="btn btn--blue">View All Events</a></p>
   </div>
 </div>
 <div class="full-width-split__two">
@@ -55,7 +64,12 @@
       </a>
       <div class="event-summary__content">
         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-        <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray"> Read more</a></p>
+        <p><?php if (has_excerpt()) {
+          echo get_the_excerpt();
+        } else {
+          echo wp_trim_words(get_the_content(), 18);
+        }
+        ?><a href="<?php the_permalink(); ?>" class="nu gray"> Read more</a></p>
       </div>
     </div>
       <?php } wp_reset_postdata();
