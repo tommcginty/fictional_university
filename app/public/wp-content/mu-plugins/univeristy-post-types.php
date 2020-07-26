b1a57175b3a6ee5ed8f432d115fb4efacb41c87d
Tom McGinty <thmcginty@gmail.com>
2020-07-26 16:13:59 -0400

    Creates Program page & archive

--- app/public/wp-content/mu-plugins/univeristy-post-types.php
+++ app/public/wp-content/mu-plugins/univeristy-post-types.php
@@ -1,6 +1,6 @@
 <?php
-
 function university_post_types() {
+    // Event post type
     register_post_type('event', array(
         'show_in_rest' => true,
         'supports' => array('title', 'editor', 'excerpt'),
@@ -16,6 +16,23 @@ function university_post_types() {
         ),
         'menu_icon' => 'dashicons-calendar'
     ));
+    // Program post type
+    register_post_type('program', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor',),
+        'rewrite' => array('slug' => 'programs'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array (
+            'name' => "Programs",
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
+    ));
+
 }
 
 add_action('init', 'university_post_types');
