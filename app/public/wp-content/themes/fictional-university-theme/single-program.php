a2eaaee3580b5a0743318af403cb3c20d4a50296
Tom McGinty <thmcginty@gmail.com>
2020-08-01 11:59:18 -0400

    Updates to program page

--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -43,6 +43,9 @@ while(have_posts()) {
           )
         )
       ));
+      if ($homepageEvents -> have_posts()) {
+        echo '<hr class="section-break">';
+      echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() .' Events</h2>';
       while($homepageEvents->have_posts()) {
         $homepageEvents->the_post(); ?>
         <div class="event-summary">
@@ -65,6 +68,7 @@ while(have_posts()) {
         </div>
       </div>
       <?php }
+      }
     ?>
 
 
