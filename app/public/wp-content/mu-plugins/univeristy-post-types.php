17ba27639468a341a03fd2a147bcc9520a37969b
Tom McGinty <thmcginty@gmail.com>
2020-08-02 11:33:17 -0400

    Adds featured image

--- app/public/wp-content/mu-plugins/univeristy-post-types.php
+++ app/public/wp-content/mu-plugins/univeristy-post-types.php
@@ -37,7 +37,7 @@ function university_post_types() {
         // Professor post type
         register_post_type('professor', array(
             'show_in_rest' => true,
-            'supports' => array('title', 'editor',),
+            'supports' => array('title', 'editor', 'thumbnail'),
             'public' => true,
             'labels' => array (
                 'name' => "Professors",
