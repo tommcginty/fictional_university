57810d76e834552decf9e26ebe783d0da54d39d7
Tom McGinty <thmcginty@gmail.com>
2020-08-08 14:03:47 -0400

    Changes to google maps function

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -34,7 +34,7 @@ function university_files() {
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
-  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyAF-_x3qSaVXeydy5QPXAnHqCsENsb57OQ', NULL, '1.0', true);
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_KEY', NULL, '1.0', true);
 
 
     if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
@@ -85,7 +85,7 @@ function university_adjust_queries($query) {
 add_action('pre_get_posts', 'university_adjust_queries');
 
 function universityMapKey($api) {
-  $api['key'] = 'AIzaSyAF-_x3qSaVXeydy5QPXAnHqCsENsb57OQ';
+  $api['key'] = YOUR_GOOGLE_MAPS_KEY; // ADD YOUR MAP KEY HERE!
   return $api;
 }
 
