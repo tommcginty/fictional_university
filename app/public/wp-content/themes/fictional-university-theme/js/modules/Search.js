ebf893d0a32039ead7b29f1f85654800ed23c94e
Tom McGinty <thmcginty@gmail.com>
2020-08-08 13:54:42 -0400

    Updates to search function

--- app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -6,8 +6,10 @@ class Search {
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
+        this.searchField = $("#search-term");
         this.events();
         this.isOverlayOpen = false;
+        this.typingTimer;
     }
 
     // 2. events
@@ -15,9 +17,24 @@ class Search {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
         $(document).on("keydown", this.keyPressDispatcher.bind(this));
+        this.searchField.on("keydown", this.typingLogic.bind(this));
     }
 
     // 3. methods
+    typingLogic() {
+        clearTimeout(this.typingTimer);
+        this.typingTimer = setTimeout(function () {console.log("test timeout")}, 2000);
+    }
+
+    keyPressDispatcher(e) {
+        if (e.keyCode == 83 && !this.isOverlayOpen) {
+            this.openOverlay();
+        }
+
+        if (e.keyCode == 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+        }
+    }
     openOverlay() {
         this.searchOverlay.addClass("search-overlay--active");
         $("body").addClass("body-no-scroll");
@@ -32,15 +49,6 @@ class Search {
         this.isOverlayOpen = false;
     }
 
-    keyPressDispatcher(e) {
-        if (e.keyCode == 83 && !this.isOverlayOpen) {
-            this.openOverlay();
-        }
-
-        if (e.keyCode == 27 && this.isOverlayOpen) {
-            this.closeOverlay();
-        }
-    }
 }
 
 export default Search;
